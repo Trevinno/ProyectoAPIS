3dfddaeefbc9c7d010b701c3694d656c8f280b82
Trevinno <josericardo.trdz@gmail.com>
2021-05-29 13:18:19 -0500

    Added last changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,10 +7,8 @@ const Homepage = () => {
         <React.Fragment>
             <body className='background'>
                 <section class="titles">
-
                     <h1>Espe</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam maiores consectetur, laboriosam rerum obcaecati.</p>
-
+                    <p>Espe es un ecosistema que busca ayudar a sus usuarios a combatir sus emociones negativas a travez de divesros sevicios. En Espe sabemos que la vida puede ser dificil y no siempre podemos lidear con nuestras problemas solos. Atravez de los servicios que ofrecemos, intentamos brindar un cambio a la persona. Ofrecemos 4 recusros de ayuda, los cuales son: un ChatRoom para chatear con usuarios y expertos, un ChatBot al cual puedas deshaogarte y contar para caulquier ayuda emocional y un foro de publicacions donde nuestros usuarios publican mensajes he historias inspiradoras que podrian ayudar a cualuqira.</p>
                 </section>
 
             <section class="container-boxes">
@@ -22,8 +20,8 @@ const Homepage = () => {
 
                 <div class="text">
                     <a href="">
-                    <h3>The Teacher</h3>
-                    <p>The best teacher you can have, internationally renowned "hooker".</p>
+                    <h3>ChatRoom</h3>
+                    <p>Aqui podras Chatear con otros usuarios y expertos que estaran dispuestos a ayudarte en cualquier cosa.</p>
                     </a>
                 </div>
 
@@ -37,8 +35,8 @@ const Homepage = () => {
 
                 <div class="text">
                     <a href="">
-                    <h3>The Yarn</h3>
-                    <p>We will use only animal-friendly, fair-trade yarn. Only the best!</p>
+                    <h3>Chatbot</h3>
+                    <p>Aqui podras tener conversaciones completamente privadas con tu amigo virtual, el cual su objetivo es que te sientas mejor.</p>
                     </a>
                 </div>
 
@@ -52,8 +50,8 @@ const Homepage = () => {
 
                 <div class="text">
                     <a href="">
-                    <h3>The Group</h3>
-                    <p>Become a member, become family. Teamwork is everything.</p>
+                    <h3>Mensajes Positivos</h3>
+                    <p>Aqui podras ver todas los mensajes positivos que han publicado nuestros usuarios para que te hagan ver un lado positivo de todo</p>
                     </a>
                 </div>
 
